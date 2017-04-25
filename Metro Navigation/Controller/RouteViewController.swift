ca310ccf8aa6b525fe90dee7568283c95aaa8c94
Vladislav Khambir <vlad.khambir@gmail.com>
2017-04-25 06:52:26 +0300

    Removed Main storyboard and ViewController / Created RouteViewController class and storyboard.

---
+++ Metro Navigation/Controller/RouteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RouteViewController.swift
+//  Metro Navigation
+//
+//  Created by Vladislav Khambir on 4/25/17.
+//  Copyright © 2017 Vladislav Khambir. All rights reserved.
+//
+
+import UIKit
+
+class RouteViewController: UIViewController {
+    
+    // MARK: - UIViewController functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
