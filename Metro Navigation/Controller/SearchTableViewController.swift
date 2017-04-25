0e51bdd2ec46ae8b2698a12f6f28c687c40c86f5
Vladislav Khambir <vlad.khambir@gmail.com>
2017-04-25 06:55:30 +0300

    Created SearchTableViewController class and storyboard.

---
+++ Metro Navigation/Controller/SearchTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTableViewController.swift
+//  Metro Navigation
+//
+//  Created by Vladislav Khambir on 4/25/17.
+//  Copyright © 2017 Vladislav Khambir. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+    
+    // MARK: - UIViewController functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
