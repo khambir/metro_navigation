16c13edb58df7e3fa1341296d9cd0bb6fd2f200a
Vladislav Khambir <vlad.khambir@gmail.com>
2017-04-26 23:25:26 +0300

    Added geolocationManager into SearchTableViewController class.

--- Metro Navigation/Controller/SearchTableViewController.swift
+++ Metro Navigation/Controller/SearchTableViewController.swift
@@ -6,17 +6,23 @@
 //  Copyright © 2017 Vladislav Khambir. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 class SearchTableViewController: UITableViewController {
     
-    // MARK: - Outlets
+    // MARK: - Properties
+    private var geolocationManager = GeolocationManager()
+    
+    // MARK: - Outlet
     @IBOutlet var searchBar: UISearchBar!
     
     // MARK: - UIViewController functions
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.titleView = searchBar
+        geolocationManager.startLocationManager()
+        geolocationManager.geolocationManagerDelegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -37,6 +43,14 @@ extension SearchTableViewController {
 
 // MARK: - UISearchBarDelegate
 extension SearchTableViewController: UISearchBarDelegate {
+    
+}
+
+extension SearchTableViewController: GeolocationManagerDelegate {
+
+    func geolocationManager(_ geolocationManager: GeolocationManager, receivedNewLocation location: CLLocation) {
+        print(location)
+    }
     
 }
  
