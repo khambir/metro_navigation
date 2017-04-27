4061528b1cf68b02c9b399b6f6aefff329d6ba5d
Vladislav Khambir <vlad.khambir@gmail.com>
2017-04-27 17:36:35 +0300

    Implemented metro station search.

--- Metro Navigation/Controller/SearchTableViewController.swift
+++ Metro Navigation/Controller/SearchTableViewController.swift
@@ -13,9 +13,16 @@ class SearchTableViewController: UITableViewController {
     
     // MARK: - Properties
     private var geolocationManager = GeolocationManager()
+    fileprivate var metroStations = MetroStation.loadAll()
+    fileprivate var filteredMetroStations: [MetroStation] = []
     
     // MARK: - Outlet
     @IBOutlet var searchBar: UISearchBar!
+    
+    // MARK: - Methods
+    private func initCell() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
     
     // MARK: - UIViewController functions
     override func viewDidLoad() {
@@ -41,15 +48,46 @@ extension SearchTableViewController {
     
 }
 
-// MARK: - UISearchBarDelegate
+// MARK: - UISearchBarDelegate functions
 extension SearchTableViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredMetroStations = metroStations.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        tableView.reloadData()
+    }
     
 }
 
+// MARK: - GeolocationManagerDelegate functions
 extension SearchTableViewController: GeolocationManagerDelegate {
 
     func geolocationManager(_ geolocationManager: GeolocationManager, receivedNewLocation location: CLLocation) {
         print(location)
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+extension SearchTableViewController {
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+        let dataSource = filteredMetroStations.isEmpty ? metroStations : filteredMetroStations
+        cell.textLabel?.setText(dataSource[indexPath.row].name, withBoldPart: searchBar.text ?? "")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredMetroStations.isEmpty ? metroStations.count : filteredMetroStations.count
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension SearchTableViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
