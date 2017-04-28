e2ff245c9fde0034bbecad62c4335ae1c010652c
Vladislav Khambir <vlad.khambir@gmail.com>
2017-04-28 14:59:54 +0300

    Added extension for UITableView class: hideBottomEmptyCells.

---
+++ Metro Navigation/Tools/UITableViewExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewExtension.swift
+//  Metro Navigation
+//
+//  Created by Vladislav Khambir on 4/28/17.
+//  Copyright © 2017 Vladislav Khambir. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func hideBottomEmptyCells() {
+        tableFooterView = UIView()
+    }
+    
+}
