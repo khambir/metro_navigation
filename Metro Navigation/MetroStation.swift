5090e56ea14ce509cd426e1324d15a50c9f24817
Vladislav Khambir <vlad.khambir@gmail.com>
2017-05-01 20:32:32 +0300

    Refactoring MetroStation.

--- Metro Navigation/MetroStation.swift
+++ Metro Navigation/MetroStation.swift
@@ -9,37 +9,21 @@
 import CoreLocation
 import UIKit
 
-struct MetroStation {
+class MetroStation: Vertex {
     
     // MARK: - Properties
-    internal let id: Int
+    internal var id: Int?
     internal let name: String
     internal let color: UIColor
     internal let location: CLLocation
+    internal var edges: [Edge] = []
     
-    // MARK: - Methods
-    internal static func loadAll() -> [MetroStation] {
-        
-        var result: [MetroStation] = []
-        
-        guard let path = Bundle.main.path(forResource: "kyivMetroStations", ofType: "plist") else { return [] }
-        //If your plist contain root as Array
-        guard let stations = NSArray(contentsOfFile: path) as? [[String: Any]] else { return [] }
-        
-        for station in stations {
-            guard   let id = station["id"] as? Int,
-                let name = station["name"] as? String,
-                let color = UIColor(hex: station["lineColor"] as? String ?? ""),
-                let location = station["location"] as? [String: Any],
-                let latitude = location["latitude"] as? Double,
-                let longitude = location["longitude"] as? Double else {
-                    continue
-            }
-            let newStation = MetroStation(id: id, name: name, color: color, location: CLLocation(latitude: latitude, longitude: longitude))
-            result.append(newStation)
-        }
-        
-        return result
+    // MARK: - Initializer
+    init(id: Int, name: String, color: UIColor, location: CLLocation) {
+        self.id = id
+        self.name = name
+        self.color = color
+        self.location = location
     }
     
 }
