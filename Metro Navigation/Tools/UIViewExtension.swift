e1a6dbb3b13dd7c11d46dadc797877e793fc456e
Vladislav Khambir <vlad.khambir@gmail.com>
2017-05-02 15:01:43 +0300

    Extended UIView class by addParallaxEffect method.

--- Metro Navigation/Tools/UIViewExtension.swift
+++ Metro Navigation/Tools/UIViewExtension.swift
@@ -18,4 +18,20 @@ extension UIView {
         layer.shadowRadius = 5
     }
     
+    func addParallaxEffect() {
+        let value = 20
+        
+        let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        horizontal.minimumRelativeValue = -value
+        horizontal.maximumRelativeValue = value
+        
+        let vertical = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        vertical.minimumRelativeValue = -value
+        vertical.maximumRelativeValue = value
+        
+        let group = UIMotionEffectGroup()
+        group.motionEffects = [horizontal, vertical]
+        addMotionEffect(group)
+    }
+    
 }
