5b6614b2fe424a41d661c7a111d27e747e80165b
Vladislav Khambir <vlad.khambir@gmail.com>
2017-05-02 09:36:53 +0300

    Added extension for Date class: timeString.

---
+++ Metro Navigation/Tools/DateExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DateExtension.swift
+//  Metro Navigation
+//
+//  Created by Vladislav Khambir on 5/2/17.
+//  Copyright © 2017 Vladislav Khambir. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+
+    internal var timeString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .short
+        return dateFormatter.string(from: self)
+    }
+    
+}
